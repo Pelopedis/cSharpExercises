9cee3fa705510b709705ec3e3ee9a5f2b91bf2e9
Chris Mappouras <57260161+Pelopedis@users.noreply.github.com>
2021-05-13 10:10:45 +0300

    Create HelloWorld.cs

---
+++ HelloWorld/HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HrlloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
